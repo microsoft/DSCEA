7d9ad829db14a52ae56f440dda92faf901379e00
Keith Hitchcock <krjhitch@gmail.com>
2017-02-19 22:05:23 -0500

    formatting improvements

--- configs/SampleConfig.ps1
+++ configs/SampleConfig.ps1
@@ -1,72 +1,68 @@
 configuration MemberServerSecuritySettings {
+    param([string[]]$ComputerName='localhost')
 
-    param(
-        [string[]]$ComputerName="localhost"
-    )
     Import-DscResource -ModuleName PSDesiredStateConfiguration
+    
     Node $ComputerName {   
-    
-    #Anti-Malware
-        Service MicrosoftAntimalwareService{
-        Name = "MsMpSvc"
-        StartupType = "Automatic"
-        State = "Running"
+
+        #Anti-Malware
+        Service 'MicrosoftAntimalwareService' {
+            Name        = 'MsMpSvc'
+            StartupType = 'Automatic'
+            State       = 'Running'
         }
-    
-    #User Account Control
-        Registry ConsentPromptBehaviorAdmin{
-        Ensure = "Present"
-        Key = "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System"
-        ValueName = "ConsentPromptBehaviorAdmin"
-        ValueType = "Dword"
-        ValueData = "5"
+
+        #User Account Control - (1 of 2)
+        Registry 'ConsentPromptBehaviorAdmin' {
+            Ensure    = 'Present'
+            Key       = 'HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System'
+            ValueName = 'ConsentPromptBehaviorAdmin'
+            ValueType = 'Dword'
+            ValueData = '5'
         }  
 
-        Registry PromptOnSecureDesktop{
-        Ensure = "Present"
-        Key = "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System"
-        ValueName = "PromptOnSecureDesktop"
-        ValueType = "Dword"
-        ValueData = "1"
+        #User Account Control - (2 of 2)
+        Registry 'PromptOnSecureDesktop' {
+            Ensure    = 'Present'
+            Key       = 'HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System'
+            ValueName = 'PromptOnSecureDesktop'
+            ValueType = 'Dword'
+            ValueData = '1'
         }
 
-	#Interactive logon: Number of previous logons to cache (in case domain controller is not available)
-        Registry Numberofpreviouslogonstocache{
-        Ensure = "Present"
-        Key = "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Winlogon"
-        ValueName = "CachedLogonsCount"
-        ValueType = "Dword"
-        ValueData = "2"
+        #Interactive logon: Number of previous logons to cache (in case domain controller is not available)
+        Registry 'Numberofpreviouslogonstocache' {
+            Ensure    = 'Present'
+            Key       = 'HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Winlogon'
+            ValueName = 'CachedLogonsCount'
+            ValueType = 'Dword'
+            ValueData = '2'
         }
 
-    #Checks to ensure that certain Windows Roles or Windows Features have not been installed
-        WindowsFeature ActiveDirectoryDomainServices{
-            Name = 'AD-Domain-Services'
-            Ensure = 'Absent'
-        }
-	
-		WindowsFeature DNSServer{
-            Name = 'DNS'
-            Ensure = 'Absent'
-        }
-    
-	    WindowsFeature DHCPServer{
-            Name = 'DHCP'
+        #Checks to ensure that certain Windows Roles or Windows Features have not been installed
+        WindowsFeature 'ActiveDirectoryDomainServices' {
+            Name   = 'AD-Domain-Services'
             Ensure = 'Absent'
         }
 
-		WindowsFeature WindowsRoleFax{
-            Name = 'Fax'
+        #Ensure DNS service is not installed
+        WindowsFeature 'DNSServer' {
+            Name   = 'DNS'
             Ensure = 'Absent'
         }
 
-		#WindowsFeature TelnetServer {
-        #    Name = 'Telnet-Server'
-        #    Ensure = 'Absent'
-        #}
-
+        #Ensure DHCP service is not installed
+        WindowsFeature 'DHCPServer' {
+            Name   = 'DHCP'
+            Ensure = 'Absent'
         }
-		        
+
+        #Ensure FaxServer service is not installed
+        WindowsFeature 'WindowsRoleFax' {
+            Name   = 'Fax'
+            Ensure = 'Absent'
+        }
+    }            
 }
 
 MemberServerSecuritySettings
