f61a67c9956cd81f955f83f757ba7e5834a6e578
Keith Hitchcock <krjhitch@gmail.com>
2017-03-04 19:13:57 -0500

    merged helper function into report code

--- functions/Get-DscEaReport.ps1
+++ functions/Get-DscEaReport.ps1
@@ -68,14 +68,20 @@ This command returns true/false per configuration item, per machine
 
         [String]$OutPath = "$env:ProgramFiles\DSC-EA\Output"
     )
-    helper-EnsureLogo
+    if(-not (Test-Path 'C:\ProgramData\DSCEA\logo.png')) {
+        $env:PSModulePath -split ';' | ForEach-Object {
+            if(Test-Path (Join-Path $_ 'DSCEA\resources\logo.png')) {
+                Copy-Item (Join-Path $_ 'DSCEA\resources\logo.png') 'C:\ProgramData\DSCEA\logo.png' -Force
+            }
+        }
+    }
     $results = Import-Clixml $InFile
     $date = (Get-ChildItem $InFile).LastWriteTime
     if($Overall){
         $results | 
         select-object -ExpandProperty Compliance | Where-Object {$_.PSComputerName -ne $null} |
         select-object @{Name="Computer";Expression={$_.PSComputerName}}, @{Name="Compliant";Expression={$_.InDesiredState}} |
-        ConvertTo-HTML -Head $webstyle -body "<img src='logo.png'/><br>","<titlesection>DSC Configuration Report</titlesection><br>","<datesection>Report last run on",$date,"</datesection><p>" | 
+        ConvertTo-HTML -Head $webstyle -body "<img src='C:\ProgramData\DSCEA\logo.png/><br>","<titlesection>DSC Configuration Report</titlesection><br>","<datesection>Report last run on",$date,"</datesection><p>" | 
         Out-File $env:ProgramFiles\DSC-EA\Output\OverallComplianceReport.html
         Write-Host "Report $OutPath\OverallComplianceReport.html generated"
     }
@@ -85,7 +91,7 @@ This command returns true/false per configuration item, per machine
         $_.ResourcesNotInDesiredState | 
         Select-Object @{Name="Computer";Expression={$_.PSComputerName}}, ResourceName, InstanceName, InDesiredState
         }
-    } | ConvertTo-HTML -Head $webstyle -body "<img src='logo.png'/><br>","<titlesection>DSC Configuration Report</titlesection><br>","<datesection>Report last run on",$date,"</datesection><p>" | 
+    } | ConvertTo-HTML -Head $webstyle -body "<img src='C:\ProgramData\DSCEA\logo.png'/><br>","<titlesection>DSC Configuration Report</titlesection><br>","<datesection>Report last run on",$date,"</datesection><p>" | 
     Out-File $env:ProgramFiles\DSC-EA\Output\DetailedComplianceReport.html 
     Write-Host "Report $OutPath\DetailedComplianceReport.html generated"
     }
@@ -96,7 +102,7 @@ This command returns true/false per configuration item, per machine
                 $_.ResourcesNotInDesiredState | ForEach-Object {$_ | Select-Object @{Name="Computer";Expression={$_.PSComputerName}}, ResourceName, InstanceName, InDesiredState}
             }
         } | Where-object {$_.InstanceName -ieq $ItemName} | 
-        ConvertTo-HTML -Head $webstyle -body "<img src='logo.png'/><br>","<titlesection>DSC Configuration Report</titlesection><br>","<datesection>Report last run on",$date,"</datesection><p>" | 
+        ConvertTo-HTML -Head $webstyle -body "<img src='C:\ProgramData\DSCEA\logo.png'/><br>","<titlesection>DSC Configuration Report</titlesection><br>","<datesection>Report last run on",$date,"</datesection><p>" | 
         Out-File $OutPath\ItemComplianceReport-$ItemName.html
         Write-Host "Report $OutPath\ItemComplianceReport-$ItemName.html generated"
     }
@@ -106,7 +112,7 @@ This command returns true/false per configuration item, per machine
                 $_.ResourcesNotInDesiredState | Select-Object @{Name="Computer";Expression={$_.PSComputerName}}, ResourceName, InstanceName, InDesiredState
                 $_.ResourcesInDesiredState | Select-Object @{Name="Computer";Expression={$_.PSComputerName}}, ResourceName, InstanceName, InDesiredState
             }
-        } | ConvertTo-HTML -Head $webstyle -body "<img src='logo.png'/><br>","<titlesection>DSC Configuration Report</titlesection><br>","<datesection>Report last run on",$date,"</datesection><p>" | Out-File $OutPath\ComputerComplianceReport-$ComputerName.html 
+        } | ConvertTo-HTML -Head $webstyle -body "<img src='C:\ProgramData\DSCEA\logo.png'/><br>","<titlesection>DSC Configuration Report</titlesection><br>","<datesection>Report last run on",$date,"</datesection><p>" | Out-File $OutPath\ComputerComplianceReport-$ComputerName.html 
         Write-Host "Report $OutPath\ComputerComplianceReport-$ComputerName.html generated"
     }
 }
