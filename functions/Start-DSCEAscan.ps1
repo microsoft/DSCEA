a421bcf314dbbdd3e6242fb102825bdf8dece3aa
Keith Hitchcock <krjhitch@gmail.com>
2017-04-04 13:39:00 -0400

    enable scriptblock private functions

--- functions/Start-DSCEAscan.ps1
+++ functions/Start-DSCEAscan.ps1
@@ -148,29 +148,11 @@ param
             [Microsoft.Management.Infrastructure.CimSession]$CimSession
             )
 
-            function Repair-DSCEngine {
-                [CmdletBinding()]
-                param
-                (
-                    [ValidateNotNullOrEmpty()]
-                    [string[]]$ComputerName
-                )
+        $functionRoot = Join-Path -Path $PSScriptRoot -ChildPath 'Functions' -Resolve
 
-                #kill the dsc processes on the remote system
-                Invoke-Command -ComputerName $ComputerName -ScriptBlock {
-        
-                    ### find the process that is hosting the DSC engine
-                    $dscProcess = Get-WmiObject msft_providers | 
-                    Where-Object {$_.provider -like 'dsccore'} | 
-                    Select-Object -ExpandProperty HostProcessIdentifier 
-        
-                    ### Stop the process
-                    do {
-                        $processID = Get-Process -Id $dscProcess
-                        $processID | Stop-Process -Force}
-                    while ($processID.ProcessName -match "WmiPrvSE")
-                } -ErrorAction SilentlyContinue
-            }
+        Get-ChildItem -Path $functionRoot -Filter '*.ps1' | ForEach-Object {
+            . $_.FullName | Out-Null
+        }   
 
         $runTime = Measure-Command {
             try
