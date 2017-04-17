d6e5da7ddb5446301c223f241e24d43e1523a301
rkyttle <ralph.kyttle@microsoft.com>
2017-04-17 14:08:34 -0400

    Updated logic for SampleConfigWithScriptResource

--- configs/SampleConfigWithScriptResource.ps1
+++ configs/SampleConfigWithScriptResource.ps1
@@ -7,13 +7,22 @@ configuration ScriptResourceSample {
             GetScript = {
                 @{
                     Results = (Get-NetFirewallRule -DisplayGroup 'Remote Desktop').Enabled
-                } #Returns a HashTable @{}
+                }
+                #Returns a HashTable @{}
             }
             TestScript = {
-                ((Get-NetFirewallRule -DisplayGroup 'Remote Desktop').Enabled).Contains('False') #Returns True or False
+                $state = Invoke-Expression $GetScript
+                if( $state.results -Contains("True") ) { #We do not want any of the Remote Desktop firewall rules enabled, so if any of the rules return a value of True, we want the test to fail
+                    return $false
+                }
+                else {
+                    return $true
+                }
+                #Returns True or False on whether the system is in the desired state
             }
             SetScript = {
-                Set-NetFirewallRule -DisplayGroup 'Remote Desktop' -Enabled True #Makes it so!
+                Set-NetFirewallRule -DisplayGroup 'Remote Desktop' -Enabled False
+                #Makes it so!
             }
         }
     }    
